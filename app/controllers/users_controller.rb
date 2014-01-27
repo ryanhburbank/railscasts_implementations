d863b1fdf643d1139bc8c7999a40e25acb3eb334
ryanhburbank <ryanhburbank@gmail.com>
2014-01-26 17:37:13 -0800

    implement cast 3 by using find through association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
 		# @user = User.find(:first, :conditions => ['gender=?', "male"], :order => 'created_at DESC')
 		@user = Task.find_by_gender("male", :order => "created_at DESC") #dynamic_find_by_field
 	end
+
+	def show
+		@user = User.find(params[:id])
+		#@posts = Post.find(:all, :conditions => ["user_id = ? AND title = ?", @user.id, "Where the Red Fern Grows"])
+		@posts = @user.posts.find_all_by_title("Where the Red Fern Grows")
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127011439) do
+ActiveRecord::Schema.define(version: 20140127012719) do
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
